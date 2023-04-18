58a959ba1db9fc084ef37d201dc43dd8fe0ef22c
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-04-18 09:46:38 +0300

    changes

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,27 +1,16 @@
-function taskFirst() {
-    const name = "John";
-    const age = 30;
-    const city = "New York";
+export const taskFirst = () => {
+    const task = 'I prefer const when I can.';
+    return task;
+  };
   
-    console.log(`My name is ${name}, I'm ${age} years old, and I live in ${city}.`);
-  }
+  export const getLast = () => {
+    return ' is okay';
+  };
   
-  function taskNext() {
-    let count = 0;
-    let isRunning = true;
-    let message = "";
+  export const taskNext = () => {
+    let combination = 'But sometimes let';
+    combination += getLast();
   
-    while (isRunning) {
-      message = `The count is ${count}.`;
-  
-      if (count >= 5) {
-        message += " The loop has ended.";
-        isRunning = false;
-      }
-  
-      console.log(message);
-  
-      count++;
-    }
-  }
+    return combination;
+  };
   
