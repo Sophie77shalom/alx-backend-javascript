aeddd134a8ff706d041fc80fb077c3aec97d0be6
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-04-21 16:04:07 +0300

    tasks

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,12 +1,12 @@
 export default function taskBlock(trueOrFalse) {
     let task = false;
-    const task2 = true;
-  
+    let task2 = true;
+    
     if (trueOrFalse) {
-      task = true;
-      const task2 = false;
+    task = true;
+    task2 = false;
     }
-  
+    
     return [task, task2];
-  }
+    }
   
