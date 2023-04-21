f575d537446448042aa93b3ed22dc75979c6248d
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-04-21 19:23:05 +0300

    promise

---
+++ 0x01-ES6_promise/4-user-promise.js
@@ -0,0 +1,9 @@
+function signUpUser(firstName, lastName) {
+    return new Promise((resolve, reject) => {
+      resolve({
+        firstName: firstName,
+        lastName: lastName,
+      });
+    });
+  }
+  
