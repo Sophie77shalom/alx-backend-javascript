7051d1f34e8d21736daecb855b23e1f7aaa89856
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-04-21 19:06:07 +0300

    Return a Promise using this prototype function getResponseFromAPI()

---
+++ 0x01-ES6_promise/0-promise.js
@@ -0,0 +1,8 @@
+function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      // your code to make API request here...
+    });
+  }
+  
+  export default getResponseFromAPI;
+  
