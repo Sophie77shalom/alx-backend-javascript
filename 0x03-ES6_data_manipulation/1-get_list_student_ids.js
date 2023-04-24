b9768354cd68d84e941c6e4e1ca3dc2ebd77b793
Sophie Shalom <106449659+Sophie77shalom@users.noreply.github.com>
2023-04-24 23:01:07 +0300

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,4 @@
-function getListStudentIds(students) {
-    if (!Array.isArray(students)) {
-      return [];
-    }
-  
-    return students.map(student => student.id);
-  }
-  
+export default function getListStudentIds(arr) {
+  if (!Array.isArray(arr)) return [];
+  return arr.map((el) => el.id);
+}
