a43df611977658fbb045fbb2e9f4bbe21d135782
Sophie77shalom <sophyjeptoo999@gmail.com>
2023-04-30 12:44:21 +0300

    typescript

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,9 @@
+// Create a type RowID and set it equal to number
+export type RowID = number;
+
+
+export interface RowElement {
+    firstName: string;
+    lastName: string;
+    age?: number;
+}
